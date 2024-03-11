d7ab8eb21a6c22d95437539b96b067f7e0a57c73
Yaroslav Sedyshev <jareksedy@icloud.com>
2024-03-11 13:23:45 +0500

    Initial Commit

---
+++ KeyTest/KeyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyTestApp.swift
+//  KeyTest
+//
+//  Created by Ярослав Седышев on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct KeyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
